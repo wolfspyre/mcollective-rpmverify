53e1bb90912683821b6a0e2bbe4281279f2cb748
Wolf Noble <wnoble@datapipe.com>
2013-02-22 17:40:23 -0600

    And now it works.

--- rpmverify.rb
+++ rpmverify.rb
@@ -5,14 +5,19 @@ module MCollective
         :description => "Verify that all files in an RPM match the md5 stored in the RPMDB",
         :author      => "Wolf Noble",
         :license     => "Apache License, Version 2.0",
-        :version     => "0.0003",
+        :version     => "0.1",
         :url         => "https://github.com/wolfspyre/mcollective-rpmverify",
         :timeout     => 10
 
       action "verify" do
         validate :package, String
         package = request[:package]
-        reply["output"] = run("/bin/rpm -Vv #{package} | /bin/egrep -v '^\.\.\.\.\.\.'", :stdout => :output, :chomp => true )
+        foo=""
+        out = run("/bin/rpm -Vv #{package} |/bin/egrep -v '^\\.{8}'  ",:stdout => foo , :chomp => true)
+        if foo.empty?
+          foo="No Deviation"
+        end
+        reply["output"] = foo
       end
     end
   end
