b4d118196019d9bd9c3ee7e293b3f68c9646c27f
Wolf Noble <wnoble@datapipe.com>
2013-02-22 18:30:21 -0600

    transform into a puppet module for ease of deployment

---
+++ manifests/init.pp
@@ -0,0 +1,43 @@
+# == Class: mco-rpmverify
+#
+# A quick and dirty mcollective plugin and puppet module to verify the sanity of files installed by rpm packages
+#
+# === Parameters
+#
+# === Variables
+#
+#
+# === Examples
+#
+# class mco-rpmverify
+#
+# === Authors
+#
+# Wolf Noble <wolf@wolfspaw.com
+#
+# === Copyright
+#
+# Copyright 2013
+#
+class mco_rpmverify {
+  #set some defaults
+  File{
+    ensure => 'file',
+    group  => 'root',
+    mode   => '0640',
+    owner  => 'root',
+  }
+  file {'/opt/puppet/libexec/mcollective/mcollective/agent/rpmverify.rb':
+    source => "puppet:///modules/${module_name}/rpmverify.rb",
+  }
+  file {'/opt/puppet/libexec/mcollective/mcollective/application/rpmverify.rb':
+    source => "puppet:///modules/${module_name}/rpmverify.rb",
+  }
+  if $::fact_is_puppetmaster {
+    #only deploy the ddl if we're a puppetmaster
+    file {'/opt/puppet/libexec/mcollective/mcollective/agent/rpmverify.ddl':
+      source => "puppet:///modules/${module_name}/rpmverify.ddl",
+    }
+  }
+
+}
