b5b4b349b263253bdd7a9a1278c859ceb712af87
Wolf Noble <wnoble@datapipe.com>
2013-02-22 19:01:44 -0600

    update file perms

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,7 +24,7 @@ class mco_rpmverify {
   File{
     ensure => 'file',
     group  => 'root',
-    mode   => '0640',
+    mode   => '0644',
     owner  => 'root',
     notify => Service['pe-mcollective'],
   }
